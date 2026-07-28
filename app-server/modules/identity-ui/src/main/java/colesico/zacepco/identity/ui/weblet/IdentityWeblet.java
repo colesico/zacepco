76b7f405bf38627d4dd908fd3ba3770d5557ffc9
Ларионов Владлен Викторович <vvlarionov@hse.ru>
2026-07-28 15:51:41 +0300

    up

--- app-server/modules/identity-ui/src/main/java/colesico/zacepco/identity/ui/weblet/IdentityWeblet.java
+++ app-server/modules/identity-ui/src/main/java/colesico/zacepco/identity/ui/weblet/IdentityWeblet.java
@@ -1,6 +1,11 @@
 package colesico.zacepco.identity.ui.weblet;
 
+import colesico.framework.assist.StringUtils;
+import colesico.framework.http.HttpMethod;
+import colesico.framework.httprouter.RequestMethod;
 import colesico.framework.httprouter.Route;
+import colesico.framework.telehttp.response.DynamicResponse;
+import colesico.framework.telehttp.response.RedirectResponse;
 import colesico.framework.weblet.Weblet;
 import colesico.framework.weblet.response.ViewResponse;
 
@@ -8,7 +13,17 @@ import colesico.framework.weblet.response.ViewResponse;
 @Route("./")
 public class IdentityWeblet {
 
-    public ViewResponse login(){
+    public ViewResponse login() {
         return ViewResponse.view("$identity/ui/tmpl/Login").build();
     }
+
+    @RequestMethod(HttpMethod.POST)
+    public DynamicResponse signin(String username, String password, String redirect) {
+        // Do login
+        if (redirect == null || !redirect.startsWith("/")) {
+            redirect = "/";
+        }
+        return RedirectResponse.of(redirect).toDynamic();
+    }
+
 }
