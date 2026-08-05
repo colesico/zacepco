eb7d40c855f5b1eaa8382d060ef840aa465af12b
Ларионов Владлен Викторович <vvlarionov@hse.ru>
2026-08-05 14:14:45 +0300

    registration done

--- app-server/modules/identity-ui/src/main/java/colesico/zacepco/identity/ui/t9n/L10nOptions.java
+++ app-server/modules/identity-ui/src/main/java/colesico/zacepco/identity/ui/t9n/L10nOptions.java
@@ -8,7 +8,9 @@ public class L10nOptions extends L10nOptionsPrototype {
 
     @Override
     public void configure(Options options) {
-        options.baseName("colesico/zacepco/identity/ui/t9n/Login_{Q}").qualifiers().language("ru")
-                .baseName("colesico/zacepco/identity/ui/t9n/SignUp_{Q}").qualifiers().language("ru");
+        options.baseName("colesico/zacepco/identity/ui/t9n/Login{Q}")
+                    .qualifiers().language("ru")
+               .baseName("colesico/zacepco/identity/ui/t9n/SignUp{Q}")
+                    .qualifiers().language("ru");
     }
 }
