c7e6bc9af3115603e3ce849f1cb730acd36f4d90
vvl <vvl@vvl.net>
2026-09-09 14:10:26 +0300

    bugfix

--- app-server/modules/common-ui/src/main/java/colesico/zacepco/common/ui/restlet/Healthcheck.java
+++ app-server/modules/common-ui/src/main/java/colesico/zacepco/common/ui/restlet/Healthcheck.java
@@ -1,12 +1,16 @@
 package colesico.zacepco.common.ui.restlet;
 
+import colesico.framework.http.HttpMethod;
+import colesico.framework.httprouter.RequestMethod;
 import colesico.framework.httprouter.Route;
 import colesico.framework.restlet.Restlet;
 
 @Restlet
 @Route("./healthcheck")
 public class Healthcheck {
-    public String index(){
-        return "OK";
+
+    @RequestMethod(HttpMethod.HEAD)
+    public void index(){
     }
+
 }
