884f9e403dbaf3ae1b8b534e9e4dcd2aff530a81
Ларионов Владлен Викторович <vvlarionov@hse.ru>
2026-09-23 15:44:29 +0300

    up

--- app-server/modules/common-ui/src/main/java/colesico/zacepco/common/ui/ioc/CommonProducer.java
+++ app-server/modules/common-ui/src/main/java/colesico/zacepco/common/ui/ioc/CommonProducer.java
@@ -1,10 +1,19 @@
 package colesico.zacepco.common.ui.ioc;
 
+import colesico.framework.ioc.production.Classed;
 import colesico.framework.ioc.production.Produce;
 import colesico.framework.ioc.production.Producer;
+import colesico.framework.restlet.RestletWriter;
 import colesico.zacepco.common.ui.teleapi.CustomJsonExceptionWriter;
+import jakarta.inject.Singleton;
 
 @Producer
 @Produce(CustomJsonExceptionWriter.class)
 public class CommonProducer {
+
+    @Singleton
+    @Classed(Exception.class)
+    public RestletWriter exceptionWriter(CustomJsonExceptionWriter impl) {
+        return impl;
+    }
 }
